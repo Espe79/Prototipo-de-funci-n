d2f9c44c4fc4dac6edadb74dce6ea51f0b15c73d
Espe79 <98134562+Espe79@users.noreply.github.com>
2022-03-29 12:42:39 -0600

    Agregue código

---
+++ Función.py
@@ -0,0 +1,5 @@
+def suma2numeros(x, y):
+    return (x + y)
+x = int(input("Ingrese el primer numero:"))
+y = int(input("Ingrese el segundo número:"))
+print("La suma es:" + str(suma2numeros(x, y))) 
